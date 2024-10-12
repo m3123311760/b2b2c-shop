f9442b38f6f0df37677a22f098cf0e928b0af39a
stitch <3123311760@qq.com>
2024-10-12 11:51:29 +0800

    首页完成

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,9 +1,7 @@
 <template>
   <header class="bg-white shadow-sm">
     <div class="container mx-auto px-4 py-4">
-      <div class="flex items-center justify-between mb-4 h-16">
-        <!-- 这里可以添加logo和搜索栏 -->
-      </div>
+
       <nav class="flex justify-between items-center">
         <CategoryMenu 
           v-for="category in navigationCategories"
